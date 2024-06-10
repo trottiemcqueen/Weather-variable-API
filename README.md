681933fea5493694a6575a46caca6e479929ffb8
Trottie McQueen <mcqueen.trottie007@gmail.com>
2024-06-10 17:26:59 -0400

    added project img shots folder for screenshots of progression and a README outline

---
+++ README.md
@@ -0,0 +1,22 @@
+## A weather variable api and will also integrate with AI for more accurate weather variations along with up-to-date weather news updates. ##
+
+<h3>To view `run npm start` in the terminal</h3>
+<p>Server running on PORT 8000. Can check out sample JSON `localhost:8000/news` forms a list in an array of attr links from a resource.</p>
+`project image shots` 
+
+<hr>
+
+<div>
+  <h3>List of dependencies</h3>
+  <li>
+    <ul>axios</ul>
+    <ul>cheerio</ul>
+    <ul>nodemon</ul>
+    <ul>express</ul>
+  </li>
+</div>
+
+<p>So far these are the beginning stages of my project, please stay tuned
+as I keep you up to-date</p>
+
+<author>Trottie McQueen</author>
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 const app = express()
+const articles = []
 
 app.get('/', (req, res) => {
     res.json('Welcome to my Weather variable API')
@@ -16,10 +17,16 @@ app.get('/news', (req, res) => {
             const html = response.data
             const $ = cheerio.load(html)
 
-            $('a:contains("climate")', html).each(function () {
-               const title = $(this).text() 
+            $('a:contains("climate")', html).each(function () {//syntax for cheerio package
+               const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url
+                })
             })
-        })
+            res.json(articles)
+        }).catch((err) => console.log(err))
 });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
