04f17c097d6606e432159d735acf1b20d92bb458
Trottie McQueen <mcqueen.trottie007@gmail.com>
2024-06-09 20:39:10 -0400

    started grabbing info for the api

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ app.get('/', (req, res) => {
     res.json('Welcome to my Weather variable API')
 });
 
+app.get('/news', (req, res) => {
+    //res.json('Welcome to my Weather variable API')
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function () {
+               const title = $(this).text() 
+            })
+        })
+});
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
 
 
