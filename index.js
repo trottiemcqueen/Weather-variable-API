653b92bfcccac01887e7148b4e8b7c0478165e54
Trottie McQueen <mcqueen.trottie007@gmail.com>
2024-06-11 17:59:38 -0400

    adding several resources to grab information looping my array with the links to grab

--- index.js
+++ index.js
@@ -2,9 +2,35 @@ const PORT = 8000
 const express = require('express')// need to initialize
 const axios = require('axios')
 const cheerio = require('cheerio')
-
 const app = express()
+
+const newsp = [
+    {
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/environment/climate-change'
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis'
+    },
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change',
+    }
+]
 const articles = []
+
+newsp.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function () {
+
+            })
+        })
+})
 
 app.get('/', (req, res) => {
     res.json('Welcome to my Weather variable API')
@@ -12,21 +38,21 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
     //res.json('Welcome to my Weather variable API')
-    axios.get('https://www.theguardian.com/environment/climate-crisis')
-        .then((response) => {
-            const html = response.data
-            const $ = cheerio.load(html)
+    // axios.get('https://www.theguardian.com/environment/climate-crisis')
+    //     .then((response) => {
+    //         const html = response.data
+    //         const $ = cheerio.load(html)
 
-            $('a:contains("climate")', html).each(function () {//syntax for cheerio package
-               const title = $(this).text()
-                const url = $(this).attr('href')
-                articles.push({
-                    title,
-                    url
-                })
-            })
-            res.json(articles)
-        }).catch((err) => console.log(err))
+    //         $('a:contains("climate")', html).each(function () {//syntax for cheerio package
+    //            const title = $(this).text()
+    //             const url = $(this).attr('href')
+    //             articles.push({
+    //                 title,
+    //                 url
+    //             })
+    //         })
+    //         res.json(articles)
+    //     }).catch((err) => console.log(err))
 });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
