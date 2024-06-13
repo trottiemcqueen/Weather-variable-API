5d34a2da1319a98c5d9e98b1b59c7220a10848c6
Trottie McQueen <mcqueen.trottie007@gmail.com>
2024-06-13 16:44:18 -0400

    final code added heroku init and gitignore setup rapidapi

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const PORT = 8000
-const express = require('express')// need to initialize
-const axios = require('axios')
-const cheerio = require('cheerio')
+const PORT = process.env.PORT || 8000 // this is for deploying to heroku
+const express = require('express');// need to initialize legacy way
+const axios = require('axios');
+const cheerio = require('cheerio');
 const app = express()
 
 const newsp = [
@@ -19,7 +19,28 @@ const newsp = [
         name: 'telegraph',
         address: 'https://www.telegraph.co.uk/climate-change',
         base: 'https://www.telegraph.co.uk'
+    },
+    {
+        name: 'sun',
+        address: 'https://www.thesun.co.uk/climate-change-environment/',
+        base: ''
+    },
+    {
+        name: 'bbc',
+        address: 'https://www.bbc.co.uk/news/science_and_environment',
+        base: 'https://www.bbc.co.uk'
+    },
+    {
+        name: 'dm',
+        address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
+        base: ''
+    },
+    {
+        name: 'nyp',
+        address: 'https://www.nypost.com/tag/climate-change',
+        base: ''
     }
+
 ]
 const articles = []
 
@@ -54,14 +75,33 @@ app.get('/news', (req, res) => {
 
 
 // to get information from just one newspaprer article
-app.get('/news/:newspaperId', async (req, res) => {
+app.get('/news/:newspaperId', (req, res) => {
     const newspaperId = req.params.newspaperId
 
-    newsp.filter(newspaper => newspaper.name == newspaperId)
+    const newspaperAddress = newsp.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperBase = newsp.filter(newspaper => newspaper.name == newspaperId)[0].base
 
-    axios.get()
+    axios.get(newspaperAddress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+
+        }).catch(err => console.log(err))
+
 })
-
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
 
 
